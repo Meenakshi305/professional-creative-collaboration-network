bf0d3abfa0f7e2cc6655a8c46a0a0dd9e162257f
KadariPranav <Meenakshifluenz@gmail.com>
2026-09-21 21:09:29 +0930

    creating endpoint for use and follow and unfollow
    creating endpoint for user profile and follow and unfollow

--- collaboration/src/main/java/com/creative/collaboration/controller/UserController.java
+++ collaboration/src/main/java/com/creative/collaboration/controller/UserController.java
@@ -55,6 +55,23 @@ public class UserController {
                 )
         );
     }
+
+
+    // DELETE /api/users/{id}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void>
+    deleteUser(
+            @PathVariable Long id
+    ) {
+
+        userService.deleteUser(id);
+
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+
+
     // GET /api/users/{id}/profile
     @GetMapping("/{id}/profile")
     public ResponseEntity<ProfileResponse>
@@ -67,3 +84,90 @@ public class UserController {
                         .getProfile(id)
         );
     }
+
+
+    // PUT /api/users/{id}/profile
+    @PutMapping("/{id}/profile")
+    public ResponseEntity<ProfileResponse>
+    updateProfile(
+            @PathVariable Long id,
+            @RequestBody
+            UpdateProfileRequest request
+    ) {
+
+        return ResponseEntity.ok(
+                userService.updateProfile(
+                        id,
+                        request
+                )
+        );
+    }
+
+
+    // POST /api/users/{id}/follow
+    @PostMapping("/{id}/follow")
+    public ResponseEntity<Void>
+    followUser(
+            @PathVariable Long id,
+
+            @RequestHeader(
+                    "X-User-Id"
+            )
+            Long loggedInUserId
+    ) {
+
+        userService.followUser(
+                loggedInUserId,
+                id
+        );
+
+
+        return ResponseEntity
+                .status(
+                        HttpStatus.CREATED
+                )
+                .build();
+    }
+
+
+    // DELETE /api/users/{id}/unfollow
+    @DeleteMapping("/{id}/unfollow")
+    public ResponseEntity<Void>
+    unfollowUser(
+            @PathVariable Long id,
+
+            @RequestHeader(
+                    "X-User-Id"
+            )
+            Long loggedInUserId
+    ) {
+
+        userService.unfollowUser(
+                loggedInUserId,
+                id
+        );
+
+
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+
+
+    // GET /api/users/{id}/followers
+    @GetMapping("/{id}/followers")
+    public ResponseEntity<
+            List<PublicUserResponse>
+            >
+    getFollowers(
+            @PathVariable Long id
+    ) {
+
+        return ResponseEntity.ok(
+                userService
+                        .getFollowers(id)
+        );
+    }
+
+
+}
