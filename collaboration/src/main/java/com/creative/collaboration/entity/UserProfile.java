037d34aa4718904f5934f0e859ecc9e083f57ff1
Khushi Patel <138774613+Khuship26@users.noreply.github.com>
2026-09-21 20:48:31 +0930

    creating user dashboard api

---
+++ collaboration/src/main/java/com/creative/collaboration/entity/UserProfile.java
@@ -0,0 +1,36 @@
+package com.creative.collaboration.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user_profiles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(
+            name = "user_id",
+            nullable = false,
+            unique = true
+    )
+    private User user;
+
+    @Column(length = 1000)
+    private String bio;
+
+    private String skills;
+
+    private String achievements;
+
+    private String profileImageUrl;
+}
